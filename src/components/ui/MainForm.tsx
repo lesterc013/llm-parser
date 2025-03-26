fbfb2046019766071080accb1c5c551d9ee83af3
lesterchan <lesterc013@gmail.com>
2025-03-26 22:31:25 +0800

    Changed implementation to use shadcn components

--- src/components/ui/MainForm.tsx
+++ src/components/ui/MainForm.tsx
@@ -1,53 +1,54 @@
 "use client";
 
-import { SubmitHandler, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
-import { useState } from "react";
-
-// schema defines for the name of the input fields, what is it we expect so that we can validate easily; e.g. for the field named "inputArea", must be string
-const schema = z.object({
-  inputArea: z.string(),
-});
-
-// Type of the form is then inferred from the schema
-type FormFields = z.infer<typeof schema>;
+import { mainFormSchema } from "@/schemas/schemas";
+import { MainFormShape } from "@/schemas/schemas";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import { callOpenAiApi } from "@/actions/actions";
 
 export default function MainForm() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isSubmitting },
-  } = useForm<FormFields>({
+  const form = useForm<MainFormShape>({
+    resolver: zodResolver(mainFormSchema),
     defaultValues: {
-      inputArea: "Paste original message here",
+      inputArea: "",
     },
-    resolver: zodResolver(schema),
   });
 
-  const [output, setOutput] = useState("");
-
-  const onSubmit: SubmitHandler<FormFields> = async (data) => {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    console.log(data.inputArea);
-
-    // TODO: Replace with the returned value from the api call
-    setOutput(data.inputArea);
-  };
-
-  // register is used to say "for this field, im registering it as one of the names i specified in the FormFields"
   return (
-    <>
-      <form className="flex flex-col gap-y-2" onSubmit={handleSubmit(onSubmit)}>
-        <input type="text" {...register("inputArea")} />
-        {errors.inputArea && (
-          <div className="text-red-500">{errors.inputArea.message}</div>
-        )}
-        <button type="submit" disabled={isSubmitting}>
-          {isSubmitting ? "Loading..." : "Submit"}
-        </button>
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(callOpenAiApi)}
+        className="w-2/3 space-y-6"
+      >
+        <FormField
+          control={form.control}
+          name="inputArea"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Original WhatsApp Message</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Paste full WhatsApp Message here"
+                  className="resize-none"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
       </form>
-      <textarea value={output} readOnly />
-    </>
+    </Form>
   );
 }
